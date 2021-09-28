6b9c69cee2cd4768ec03c0c1cc3c374921d5ddf3
architjain1996 <architjainudr.aj@gmail.com>
2021-09-28 09:27:42 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 function checkProftLoss()
 {
+debugger;
     var buyingPrice = parseFloat(document.getElementById("priceId").value);
     var quantityBought = parseInt(document.getElementById("quantityId").value);
     var currentPrice = parseFloat(document.getElementById("currentPriceId").value);
+    if(isNaN(buyingPrice) || isNaN(quantityBought) || isNaN(currentPrice))
+    {
+        alert("Please enter all the values to proceed");
+    }
     var  result = document.getElementById("resultId");
     if(buyingPrice > currentPrice)
     {
         var loss = (buyingPrice - currentPrice) * quantityBought;
         var lossPercent = (loss / buyingPrice) * 100;
-        result.innerHTML = "You have made a loss of " + loss + " rupees / loss percent of " + lossPercent+" %";
+        result.innerHTML = "You have made a loss of " + loss + " rupees / loss percent of " + lossPercent.toFixed(2)+" %";
     }
     else
     {
         var profit = (currentPrice-buyingPrice) * quantityBought;
         var profitPercent = (profit / buyingPrice) * 100;
-        result.innerHTML = "You have made a profit of " + profit + " rupees / profit percent of " + profitPercent+" %";
+        result.innerHTML = "You have made a profit of " + profit + " rupees / profit percent of " + profitPercent.toFixed(2)+" %";
     }
 }
